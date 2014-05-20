65fcb355d3073f77702d1a691d01409c10f5afc6
unknown <ColleyR@NOTEB-043738.chisholm.edu.au>
2014-05-20 13:05:39 +1000

    + locationmanager

--- src/colley/chisholm/diploma/android_proximity_alert/MainActivity.java
+++ src/colley/chisholm/diploma/android_proximity_alert/MainActivity.java
@@ -3,12 +3,17 @@ package colley.chisholm.diploma.android_proximity_alert;
 import android.app.Activity;
 import android.app.ActionBar;
 import android.app.Fragment;
+import android.content.Context;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 import android.os.Build;
 
 public class MainActivity extends Activity {
@@ -20,7 +25,7 @@ public class MainActivity extends Activity {
 
 		if (savedInstanceState == null) {
 			getFragmentManager().beginTransaction()
-					.add(R.id.container, new PlaceholderFragment()).commit();
+			.add(R.id.container, new PlaceholderFragment()).commit();
 		}
 	}
 
@@ -49,15 +54,58 @@ public class MainActivity extends Activity {
 	 */
 	public static class PlaceholderFragment extends Fragment {
 
+		static LocationManager lm;
+		static Activity activity;
+
 		public PlaceholderFragment() {
 		}
 
 		@Override
 		public View onCreateView(LayoutInflater inflater, ViewGroup container,
 				Bundle savedInstanceState) {
+			activity = getActivity();
 			View rootView = inflater.inflate(R.layout.fragment_main, container,
 					false);
+			lm = (LocationManager)getActivity().getSystemService(Context.LOCATION_SERVICE);
+			lm.requestLocationUpdates(LocationManager.GPS_PROVIDER, 10000, 1, mLocationListener);
+
 			return rootView;
+		}
+
+		final static LocationListener mLocationListener = new LocationListener() { // http://developer.android.com/reference/android/location/LocationListener.html
+
+			@Override
+			public void onLocationChanged(Location location) {
+				respond();
+			}
+
+			@Override
+			public void onStatusChanged(String provider, int status,
+					Bundle extras) {
+				// TODO Auto-generated method stub
+
+			}
+
+			@Override
+			public void onProviderEnabled(String provider) {
+				// TODO Auto-generated method stub
+
+			}
+
+			@Override
+			public void onProviderDisabled(String provider) {
+				// TODO Auto-generated method stub
+
+			}
+
+		};
+
+		public static void respond() {
+
+			if (lm.getLastKnownLocation(LocationManager.GPS_PROVIDER) != null) {
+				Toast.makeText(activity, "Responding", Toast.LENGTH_SHORT).show();
+			}
+			
 		}
 	}
 
